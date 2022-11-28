af67b1ea7fed2277a48626022bdc0d52e60012c2
vohoangkh4ng <95470981+vohoangkh4ng@users.noreply.github.com>
2022-11-28 23:30:46 +0700

    Add files via upload

---
+++ ass.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    pair<int ,int > v ;
+    make_pair(1,2);
+    cout<<v.first<<endl;
+    cout<<v.second<<endl;
+}
